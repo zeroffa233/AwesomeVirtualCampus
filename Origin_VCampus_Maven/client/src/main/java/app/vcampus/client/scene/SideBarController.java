5c3a5a4efe024f7da7d6da6eeaafb2e6116e34a6
userofghb <2746293150@qq.com>
2025-09-15 17:21:33 +0800

    update

--- Origin_VCampus_Maven/client/src/main/java/app/vcampus/client/scene/SideBarController.java
+++ Origin_VCampus_Maven/client/src/main/java/app/vcampus/client/scene/SideBarController.java
@@ -182,14 +182,19 @@ public class SideBarController implements Initializable {
     @FXML
     private void handleStudentStatus() {
         setActiveButton(studentStatusButton);
+        List<Node> menuItems = new ArrayList<>();
         // if a user is a student
         if (FakeRepository.user != null && Arrays.asList(FakeRepository.user.getRoles()).contains("student")) {
-            switchView("/app/vcampus/client/scene/SubScene/StudentScene/StudentStatusView.fxml", "我的学籍", List.of());
-        }
+            JFXButton studentButton = createSecondaryMenuButton("我的学籍", "/app/vcampus/client/scene/SubScene/StudentScene/StudentStatusView.fxml");
+            menuItems.add(studentButton);
+            studentButton.getStyleClass().add("active");
+            }
         // if a user is an administrator
-        else {
-            switchView("/app/vcampus/client/scene/SubScene/StudentScene/StudentStatusManagementView.fxml", "学籍管理", List.of());
+        if(FakeRepository.user != null && Arrays.asList(FakeRepository.user.getRoles()).contains("student")) {
+            JFXButton studentmButton = createSecondaryMenuButton("修改学籍", "/app/vcampus/client/scene/SubScene/StudentScene/StudentStatusManagementView.fxml");
+            menuItems.add(studentmButton);
         }
+        switchView("/app/vcampus/client/scene/SubScene/StudentScene/StudentStatusView.fxml", "我的学籍", menuItems);
     }
 
     @FXML
